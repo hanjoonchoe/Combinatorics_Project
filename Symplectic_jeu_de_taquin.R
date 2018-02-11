911c4ae2d61609bc1513bfa48f1b79a77f9ce69c
hanjoonchoe <hchoe2@albany.edu>
2018-02-11 00:53:58 -0500

    Add files via upload

--- Symplectic_jeu_de_taquin.R
+++ Symplectic_jeu_de_taquin.R
@@ -1,4 +1,3 @@
-
 #Determine Shape of Tableaux
 Shape <- function(){
   
@@ -25,9 +24,6 @@ Shape <- function(){
         }
       }
     }
-
-    
-
   }
   
   skel_tableaux = matrix(,nrow=skel_lamda,ncol=lamda[1,1])
@@ -100,8 +96,7 @@ sjdt_Algorithm <- function(tableaux,range){
   
   unsplit <- tableaux
   print(unsplit)
-  uni_unsplit <-unique(unsplit)
-  #print(uni_unsplit)
+  uni_unsplit <-abs(unsplit)
   
   I <-list()
   J <-list()
@@ -114,34 +109,35 @@ sjdt_Algorithm <- function(tableaux,range){
   
   for(l in 1:ncol(unsplit)){
     
-    count = 0
+
     #full-slot
-    I[[l]] <- matrix(,nrow=1,ncol=ncol(unsplit))
+    I[[l]] <- matrix(,nrow=1,ncol=1)
     #empty-slot
-    J[[l]] <- matrix(,nrow=1,ncol=ncol(unsplit))
+    J[[l]] <- matrix(,nrow=1,ncol=1)
     
 
     for(m in 1:range){
       
       if(sum(uni_unsplit[,l]==m)==2){
-        I[[l]][1,count] <- m
-        count = count+1
+        I[[l]][1,] <- m
+
+
       }
       else if(sum(uni_unsplit[,l]==m)==0){
-        J[[l]][1,count] <- m
-        count = count+1
+        J[[l]][1,] <- m
+
       }
     }
-
+    
   }
   
-  #A
+
   A[[l]] <- matrix(,nrow=1,ncol=ncol(unsplit))
-  #B
+
   B[[l]] <- matrix(,nrow=1,ncol=ncol(unsplit))
-  #C
+
   C[[l]] <- matrix(,nrow=1,ncol=ncol(unsplit))
-  #D
+
   D[[l]] <- matrix(,nrow=1,ncol=ncol(unsplit))
   
   
@@ -155,16 +151,19 @@ sjdt_Algorithm <- function(tableaux,range){
     #Extract B and C
     B[[n]] <- subset(unsplit[,n],unsplit[,n]<0)
     C[[n]] <- subset(unsplit[,n],unsplit[,n]>0)
+    
     for(o in 1:ncol(I[[n]])){
       
       if(-I[[n]][,o] %in% B[[n]]){
         
-        for(q in 1:col(J[[n]])){
+        for(q in 1:ncol(J[[n]])){
           
-          if(I[[n]][,o]<J[[n]][,q]){
-            
+          if(I[[n]][,o]>J[[n]][,q]){
+
             B[[n]][B[[n]] == -I[[n]][,o]] <- -J[[q]]
+
             C[[n]][C[[n]] == I[[n]][,o]] <- J[[q]]
+
             
           }
         }
@@ -172,14 +171,60 @@ sjdt_Algorithm <- function(tableaux,range){
     }
     sort(B[[n]],decreasing = FALSE)
     sort(C[[n]],decreasing = FALSE)
-    
   }
   
+  split <- matrix(,nrow=nrow(unsplit),ncol=2*ncol(unsplit))
   
+  for(u in 1:ncol(unsplit)){
+    
+    combined_left <- append(A[[u]],D[[u]])
+    combined_right <- append(B[[u]],C[[u]])
+    while(length(combined_left)!=nrow(unsplit)){
+      combined_left <- append(combined_left,0)
+    }
+    while(length(combined_right)!=nrow(unsplit)){
+      combined_right <- append(combined_right,0)
+    }
+    
+    split[,2*u-1] <- combined_left
+    split[,2*u] <- combined_right
+  }
+  
+  #Movement section
+  split[split == -3] <- 0
+  index <- which(split==0,arr.ind=TRUE)
+  index <- index[order(index[,1],decreasing=TRUE),]
+  
+  index_cord <- nrow(index)
+  print(index)
+  
+  change_cord <- matrix(,nrow=1,ncol=1)
+  while(index_cord==0){
+    
+    
+    #vertical move
+    if(split[index[2*k,1],index[2*k,2]]<=split[index[2*k+1,1],index[2*k+1,2]]){
+      
+      split[index[2*k,1],index[2*k,2]] <- split[index[2*k,1],index[2*k,2]]
+      
+    }
+    else{
+      
+      if(split[index[2*k,1],index[2*k,2]]>0){
+        
+      }
+      else{
+        
+      }
+      
+    }
+    #horizontal move
+    k = k-1
+  }
 
-  print(B)
-  print(C)
-  return(J)
+  
+  print(split)
+  return(split)
 }
 
 a <- sjdt_Algorithm(Shape,Range)
